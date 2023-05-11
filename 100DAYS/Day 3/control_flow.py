aa9dc4988fde310a3e64594f61b97c9a150a0251
relESCampONes <wellington_lima@live.com>
2023-05-11 20:40:47 -0300

    add age control flow

--- 100DAYS/Day 3/control_flow.py
+++ 100DAYS/Day 3/control_flow.py
@@ -1,7 +1,12 @@
 print("Welcome to the rollercoaster!!!")
 height = float(input("What is your height? "))
+age = int(input("How old are you?"))
 
 if height >= 1.20:
     print("You can ride the rollercoaster!!!")
+    if age <=18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
 else:
     print("Wait a few years and come back!!!")
