1854fccf0452b1d0757b5a9c3b55c9352879fed3
relESCampONes <wellington_lima@live.com>
2023-05-11 20:41:27 -0300

    Introducing the Modulo

---
+++ 100DAYS/Day 3/even_or_odd.py
@@ -0,0 +1,10 @@
+# Odd or Even
+data = int(input("Which number do you want to check?"))
+
+calculate = data%2
+
+if calculate != 0:
+    print("This is an odd number!!!")
+else:
+    print("This is an even number!!!")
+    
