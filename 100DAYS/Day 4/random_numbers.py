e3a83618d92bc351f498825b388b63463e878c18
relESCampONes <wellington_lima@live.com>
2023-06-22 16:17:40 -0300

    learning about modules

---
+++ 100DAYS/Day 4/random_numbers.py
@@ -0,0 +1,12 @@
+import random
+import my_module
+
+print(my_module.pi)
+print(my_module.perfect_number)
+
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# random.random() return float numbers between 0.00000 - 0.99999
+random_float = random.random()
+print(random_float*10)
